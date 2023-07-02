8973249326021409cfc74ffddcefade2a67782be
JaxsonMontanaro <jaxsonmontanaro@gmail.com>
2023-07-02 15:14:58 -0700

    test

---
+++ tex.js
@@ -0,0 +1,12 @@
+let x = 5
+let y = 0
+
+if (x == 5) {
+    x += 5
+    y + 5
+}
+
+console.log(x)
+console.log(y)
+
+console.log(bfjhghgjfjgjjfgfgjfgfhhffggfhhgffghgfhfgfjhfgjhfgjhgfjhfgjhfghfghfghfgfhgfhhgfhfghfghfghfgfghfghfggfhgfgffgffghgfhgffgfhfghfgfhghfgfhfghfghfghfgfhghffffghfgfghfgfhghfghfghfgghjfdjhgfjhgfhjgfhgfghfhjgffhjgffgjfgjgffgfjgjfghfjfghjfgjhfgh)
---
+++ woah.js
@@ -0,0 +1,12 @@
+let root = 10;
+let left = 4;
+let right = 6;
+
+if (root = left + right) { console.log(true)}
+else { console.log(false)};
+
+
+
+Promise.all='hi'
+
+vonsole.log(promise.all)
